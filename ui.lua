869f5a391d7fb21a05bbf50eb9d9b8465e03d02f
andr-ew <andrewcshike@gmail.com>
2023-12-03 00:29:46 -0600

    latch chord replace

--- ui.lua
+++ ui.lua
@@ -1,18 +1,62 @@
 local Keymap = { grid = {} }
 
 function Keymap.grid.poly()
-   local _momentaries = Grid.momentaries()
-   local _toggles = Grid.toggles()
+    local held = {}
+    local is_releasing = false
 
-   return function(props)
+    local que = {}
+    
+    local _momentaries = Grid.momentaries()
+        
+    return function(props)
         local keymap = props.keymap
 
         props.state = crops.of_variable(keymap.keys, keymap.set_keys)
 
-        if props.mode == 'latch' then
-            _toggles(props)
+        if crops.mode == 'input' then
+            local x, y, z = table.unpack(crops.args) 
+            local idx = Grid.util.xy_to_index(props, x, y)
+
+            if idx then 
+                local latch = props.mode == 'latch'
+
+                local function chord_add(idx) 
+                    que[idx] = 1
+                end
+                local function chord_release()
+                    if latch then crops.set_state(props.state, que) end
+                    que = {}
+                end
+                local function tap(idx) if latch then 
+                    local old = crops.get_state_at(props.state, idx) or 0
+                    crops.set_state_at(props.state, idx, old ~ 1)
+                end end
+
+                if z==1 then
+                    table.insert(held, idx)
+
+                    if #held == 3 then
+                        for _,iidx in ipairs(held) do chord_add(iidx) end
+                    elseif #held > 2 then
+                        chord_add(idx)
+                    end
+                elseif z==0 then
+                    if #held < 2 and is_releasing then
+                        chord_release() 
+                        is_releasing = false
+                    elseif #held > 2 then
+                        is_releasing = true
+                    else
+                        tap(idx)
+                    end
+                    
+                    table.remove(held, tab.key(held, idx))
+                end
+
+                if not latch then _momentaries(props) end
+            end
         else
-           _momentaries(props)
+            _momentaries(props)
         end
     end
 end
